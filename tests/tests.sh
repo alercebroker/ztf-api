5038dc27bf087bc07dfa1dc59cf5c76b18b8e607
StarBrand <juan.saez.hidalgo@gmail.com>
2019-06-24 15:17:18 -0400

    A working output for get_probabilities

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,36 +1,39 @@
 #method error
-curl  localhost:8084/query
+curl  localhost:8085/query
 #malformed error
-curl -XPOST localhost:8084/query
+curl -XPOST localhost:8085/query
 #malformed error
-curl -XPOST -d ''  localhost:8084/query
+curl -XPOST -d ''  localhost:8085/query
 #all objects
-curl -XPOST -d '{"query_parameters":{}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{}}' localhost:8085/query
 #one object
-curl -XPOST -d '{"query_parameters":{"filters": {"oid":"ZTF18ablqckd"}}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{"filters": {"oid":"ZTF18ablqckd"}}}' localhost:8085/query
 #nobs
-curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"max":5}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5, "max": 10}}}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"max":5}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5, "max": 10}}}}' localhost:8085/query
 
 #No deberia retornar nada, pero no deberia caerse
-curl -XPOST -d '{"query_parameters":{"filters": {"oid":"ZTF18ablqckd","nobs":{"min":5, "max": 10}}}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{"filters": {"oid":"ZTF18ablqckd","nobs":{"min":5, "max": 10}}}}' localhost:8085/query
 
 
 #Dates
-curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"firstjd":"58000"}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"lastjd":"58300"}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"firstjd":"58000","lastjd":"58300"}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5, "max": 10},"dates":{"firstjd":"58000","lastjd":"58300"}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"min":"1"}}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"max":"5"}}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"min":"1","max":"5"}}}}}' localhost:8084/query
-curl -XPOST -d '{"query_parameters":{"filters":{"nobs":{"min":5, "max": 10},"dates":{"firstjd":"58000","lastjd":"58300","deltajd":{"min":"1","max":"5"}}}}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"firstjd":"58000"}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"lastjd":"58300"}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters": {"dates":{"firstjd":"58000","lastjd":"58300"}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters": {"nobs":{"min":5, "max": 10},"dates":{"firstjd":"58000","lastjd":"58300"}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"min":"1"}}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"max":"5"}}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters":{"dates":{"deltajd":{"min":"1","max":"5"}}}}}' localhost:8085/query
+curl -XPOST -d '{"query_parameters":{"filters":{"nobs":{"min":5, "max": 10},"dates":{"firstjd":"58000","lastjd":"58300","deltajd":{"min":"1","max":"5"}}}}}' localhost:8085/query
 
 #coordinates
-curl -XPOST -d '{"query_parameters":{"filters":{"coordinates":{"ra":"30","dec":"50","rs":"100"}}}}' localhost:8084/query
+curl -XPOST -d '{"query_parameters":{"filters":{"coordinates":{"ra":"30","dec":"50","rs":"100"}}}}' localhost:8085/query
 
 #get detections
-curl -XPOST -d '{"oid": "ZTF17aabdwqq"}' localhost:8084/get_detections
+curl -XPOST -d '{"oid": "ZTF17aabdwqq"}' localhost:8085/get_detections
 #get non_detections
-curl -XPOST -d '{"oid": "ZTF17aabdwqq"}' localhost:8084/get_non_detections
+curl -XPOST -d '{"oid": "ZTF17aabdwqq"}' localhost:8085/get_non_detections
+
+#get probabilities
+curl -XPOST -d '{"oid": "ZTF17aabdwqq"}' localhost:8085/get_probabilities
