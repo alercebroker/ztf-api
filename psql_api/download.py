3bd6566e0de6fe9d0f63626fb7a88caecaa11327
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-07-31 17:01:16 -0400

    implementing download

--- psql_api/download.py
+++ psql_api/download.py
@@ -11,16 +11,16 @@ download_blueprint = Blueprint(
     'download', __name__, template_folder='templates')
 
 psql_pool = pool.SimpleConnectionPool(0, 20, user=config["DATABASE"]["User"],
-                                              password=config["DATABASE"]["Pass"],
-                                              host=config["DATABASE"]["Host"],
-                                              port=config["DATABASE"]["Port"],
-                                              database=config["DATABASE"]["Database"])
+                                      password=config["DATABASE"]["Pass"],
+                                      host=config["DATABASE"]["Host"],
+                                      port=config["DATABASE"]["Port"],
+                                      database=config["DATABASE"]["Database"])
 
 
 @download_blueprint.route("/download", methods=("POST",))
 def download():
-    data = ['1','2','3','4','5','6','7','8','9','10']
-    def generate():
-        for row in data:
-            yield ','.join(row) + '\n'
-    return Response(generate(), mimetype='text/csv')
+    data = [[i for i in range(10)] for j in range(1000000)]
+    generator = (cell for row in data
+                 for cell in row)
+    return Response(generator, mimetype='text/plain', headers={"Content-Disposition":
+                                                             "attachment;filename=test.txt"})
