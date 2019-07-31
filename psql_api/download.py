302043cec6a9bd5756d966713cadde33b3375989
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-07-31 16:44:19 -0400

    implementing download

--- psql_api/download.py
+++ psql_api/download.py
@@ -1,21 +1,26 @@
 from psycopg2 import pool
 from .app import config
 from .query import parse_filters
-from flask import Response,stream_with_context,request,Blueprint,current_app,g,jsonify,send_from_directory
+from flask import Response, request, Blueprint, current_app, g, jsonify
 from io import StringIO
 
 import logging
 logger = logging.getLogger(__name__)
 
-download_blueprint = Blueprint('download', __name__, template_folder='templates')
+download_blueprint = Blueprint(
+    'download', __name__, template_folder='templates')
 
-psql_pool = pool.SimpleConnectionPool(0, 20,user = config["DATABASE"]["User"],
-                                              password = config["DATABASE"]["Pass"],
-                                              host = config["DATABASE"]["Host"],
-                                              port = config["DATABASE"]["Port"],
-                                              database = config["DATABASE"]["Database"])
+psql_pool = pool.SimpleConnectionPool(0, 20, user=config["DATABASE"]["User"],
+                                              password=config["DATABASE"]["Pass"],
+                                              host=config["DATABASE"]["Host"],
+                                              port=config["DATABASE"]["Port"],
+                                              database=config["DATABASE"]["Database"])
 
-@download_blueprint.route("/download",methods=("POST",))
+
+@download_blueprint.route("/download", methods=("POST",))
 def download():
-    f = StringIO("some initial text data")
-    return Response(f.getvalue(), mimetype='text/csv')
+    data = [1,2,3,4,5,6,7,8,9,10]
+    def generate():
+        for row in data:
+            yield ','.join(row) + '\n'
+    return Response(generate(), mimetype='text/csv')
