8b62c46e7091de56e988884893717744409dd711
Camilo Valenzuela <cacke91@gmail.com>
2019-09-11 20:37:34 -0300

    Doing just one query

--- psql_api/external.py
+++ psql_api/external.py
@@ -1,4 +1,4 @@
-from .app import cache, config, psql_pool
+from .app import cache, config
 from flask import Blueprint, Response, current_app, request, jsonify, stream_with_context
 from io import StringIO
 from astropy import units as u
@@ -7,6 +7,7 @@ from astropy.coordinates import SkyCoord
 import pandas as pd
 import numpy as np
 import requests
+import time
 
 
 external_blueprint = Blueprint('external', __name__, template_folder='templates')
@@ -18,55 +19,48 @@ def dourl(searchweb, searchoptions):
         url = "%s&%s=%s" % (url, key, searchoptions[key])
     return url
 
+
 def get_tns_df(searchweb,searchoptions):
     urlpage = dourl(searchweb,searchoptions)
+    t0 = time.time()
     s = requests.Session()
     response = s.get(urlpage)
     response.close()
     df = pd.read_csv(StringIO(response.text))
-    urls = [f"http://alerce.online/object/{oid}" for oid in df['Disc. Internal Name']]
-    df['url'] = urls
     df.sort_values("Discovery Date (UT)",ascending=False)
     return df
 
+
+
 @external_blueprint.route("/get_alerce_tns")
-@cache.memoize(60*60*24)
+@cache.memoize(60*60)
 def get_alerce_tns():
     searchweb = "https://wis-tns.weizmann.ac.il/search?"
     searchoptions = {
         "groupid[]":74,
         "num_page" : "1000",  # number of rows per page
         "internal_name" : "ZTF",
-        "classified_sne" : 1,
+        "classified_sne" : 0,
         "unclassified_at": 0,
+        "discovered_period_value": 1,
+        "discovered_period_units": "months",
         "format" : "csv",
         "display[remarks]":1}
 
-    classified_searchoptions = searchoptions.copy()
-    candidates_searchoptions = searchoptions.copy()
-
-    candidates_searchoptions["classified_sne"] = 0
-    candidates_searchoptions["unclassified_at"] = 1
-
-    candidates = get_tns_df(searchweb,candidates_searchoptions)
-    classified = get_tns_df(searchweb,classified_searchoptions)
+    all_alerce = get_tns_df(searchweb,searchoptions)
 
     dict_candidates = []
     dict_classified = []
 
-    for _,row in candidates.iterrows():
+    for _,row in all_alerce.iterrows():
         coords = SkyCoord("%s %s" % (row['RA'], row['DEC']), unit = (u.hourangle, u.deg), frame = 'fk5')
         row["RA"] = coords.ra.value
         row["DEC"] = coords.dec.value
         values = [None if (type(r) is float and np.isnan(r)) else r for r in row.values]
-        dict_candidates.append(dict(zip(row.keys(),values)))
-
-    for _,row in classified.iterrows():
-        coords = SkyCoord("%s %s" % (row['RA'], row['DEC']), unit = (u.hourangle, u.deg), frame = 'fk5')
-        row["RA"] = coords.ra.value
-        row["DEC"] = coords.dec.value
-        values = [None if (type(r) is float and np.isnan(r)) else r for r in row.values]
-        dict_classified.append(dict(zip(row.keys(),values)))
+        if type(row["Obj. Type"]) is not str:
+            dict_classified.append(dict(zip(row.keys(),values)))
+        else:
+            dict_candidates.append(dict(zip(row.keys(),values)))
 
     result = {
         "results":{
@@ -76,3 +70,7 @@ def get_alerce_tns():
     }
 
     return jsonify(result)
+
+
+
+# atexit.register(lambda: scheduler.shutdown(wait=False))
